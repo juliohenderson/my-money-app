a8966ba92c5bc3b5b88d0bd3e566fe339877ef0f
Julio Henderson <julio.henderson98@gmail.com>
2019-03-15 22:14:57 -0300

    feat(frontend): create Frontend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2019
+      <a href="http://linkedin.com/in/juliohenderson" target='_blank'> Júlio Henderson </a>.
+    </strong>
+  </footer>
+)
